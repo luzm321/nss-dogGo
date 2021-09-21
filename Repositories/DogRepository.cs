c0414184f9203288792bc5db2b76243e064de0d1
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-09-21 14:44:48 -0500

    added method to delete/remove a dog in DogRepository class

--- Repositories/DogRepository.cs
+++ Repositories/DogRepository.cs
@@ -176,6 +176,24 @@ namespace DogGo.Repositories
             }
         }
 
+        // DELETE: Dog
+        public void DeleteDog(int dogId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
 
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            DELETE FROM Dog
+                            WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", dogId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
