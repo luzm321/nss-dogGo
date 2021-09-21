ad243f9d0d8fc6a94812d6413ae4290cba25d951
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-09-20 21:40:29 -0500

    Completed exercise challenge to add functionality to display/list all the dogs of an owner when viewing details page; refactored GetOwnerById() method in OwnerRepository.cs and updated Owner Details Razor template view

--- Repositories/OwnerRepository.cs
+++ Repositories/OwnerRepository.cs
@@ -78,28 +78,59 @@ namespace DogGo.Repositories
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = @"SELECT o.Id, o.Email, o.Name, o.Address, o.NeighborhoodId, o.Phone, n.Id [nId], n.Name [Neighborhood Name]
+                    cmd.CommandText = @"SELECT o.Id, 
+                                               o.Email, 
+                                               o.Name, 
+                                               o.Address, 
+                                               o.NeighborhoodId, 
+                                               o.Phone, 
+                                               n.Id [nId], 
+                                               n.Name [Neighborhood Name],
+                                               d.Id [DogId],
+                                               d.Name [Dog Name],
+                                               d.Breed,
+                                               d.Notes,
+                                               d.ImageUrl [DogImageUrl]
                                       FROM Owner o
-                                      INNER JOIN Neighborhood n
+                                      LEFT JOIN Dog d
+                                      ON o.Id = d.OwnerId
+                                      INNER JOIN Neighborhood n 
                                       ON o.NeighborhoodId = n.Id
                                       WHERE o.Id = @id";
 
                     cmd.Parameters.AddWithValue("@id", id);
 
+                    Owner owner = null;
+
                     using (SqlDataReader reader = cmd.ExecuteReader())
                     {
-                        if (reader.Read())
+                        while (reader.Read())
                         {
-                            Owner owner = new Owner
+                            if (owner == null)
                             {
-                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                                Email = reader.GetString(reader.GetOrdinal("Email")),
-                                Name = reader.GetString(reader.GetOrdinal("Name")),
-                                Address = reader.GetString(reader.GetOrdinal("Address")),
-                                NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
-                                Phone = reader.GetString(reader.GetOrdinal("Phone"))
-                            };
-
+                                owner = new Owner
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                    Email = reader.GetString(reader.GetOrdinal("Email")),
+                                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                                    Address = reader.GetString(reader.GetOrdinal("Address")),
+                                    NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                                    Phone = reader.GetString(reader.GetOrdinal("Phone")),
+                                    Dogs = new List<Dog>()
+                                };
+                            }
+                            // If there is a DogId in the database:
+                            if (!reader.IsDBNull(reader.GetOrdinal("DogId")))
+                            {
+                                owner.Dogs.Add(new Dog
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("DogId")),
+                                    Name = reader.GetString(reader.GetOrdinal("Dog Name")),
+                                    Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                                    //Notes = reader.GetString(reader.GetOrdinal("Notes")),
+                                    //ImageUrl = reader.GetString(reader.GetOrdinal("DogImageUrl"))
+                                });
+                            }
                             // If there is a NeighborhoodId in the database:
                             if (!reader.IsDBNull(reader.GetOrdinal("NeighborhoodId")))
                             {
@@ -109,13 +140,13 @@ namespace DogGo.Repositories
                                     Name = reader.GetString(reader.GetOrdinal("Neighborhood Name"))
                                 };
                             }
-
-                            return owner;
+                            else
+                            {
+                                return null;
+                            }
                         }
-                        else
-                        {
-                            return null;
-                        }
+        
+                        return owner;
                     }
                 }
             }
