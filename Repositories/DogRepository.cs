0024e6ccdff739db9c6bc5eca8527905c225c52d
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-09-21 14:06:46 -0500

    created DogRepository class and IDogRepository interface; added method to get list of all dogs

---
+++ Repositories/DogRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace DogGo.Repositories
+{
+    public class DogRepository : IDogRepository
+    {
+        private readonly IConfiguration _config;
+
+        // The constructor accepts an IConfiguration object as a parameter. This class comes from the ASP.NET framework and is useful for retrieving things out of the appsettings.json file like connection strings.
+        public DogRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET/List: All Dogs in db:
+        public List<Dog> GetAllDogs()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT d.Id, d.[Name], d.OwnerId, d.Breed, ISNULL(d.Notes, 'No Notes') [Notes], ISNULL(d.ImageUrl, 'No Image') [DogImageUrl], o.Name [Owner Name]
+                                      FROM Dog d
+                                      LEFT JOIN Owner o
+                                      ON d.OwnerId = o.Id";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Dog> dogs = new List<Dog>();
+                        while (reader.Read())
+                        {
+                            Dog dog = new Dog
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                                Notes = reader.GetString(reader.GetOrdinal("Notes")),
+                                ImageUrl = reader.GetString(reader.GetOrdinal("DogImageUrl")),
+                                Owner = new Owner
+                                {
+                                    Name = reader.GetString(reader.GetOrdinal("Owner Name"))
+                                }
+                            };
+
+                            dogs.Add(dog);
+                        }
+
+                        return dogs;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
