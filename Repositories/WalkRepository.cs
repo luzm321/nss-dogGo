a01a96c83f0e76c9b02298b5727ac3b5c77820b6
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-09-22 14:49:15 -0500

    added method to get a walk by its id and a method to create/add a walk

--- Repositories/WalkRepository.cs
+++ Repositories/WalkRepository.cs
@@ -80,5 +80,102 @@ namespace DogGo.Repositories
                 }
             }
         }
+
+        // GET: Walk by Id
+        public Walk GetWalkById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT w.Id, 
+                                               w.Date, 
+                                               w.WalkerId, 
+                                               w.DogId, 
+                                               w.Duration
+                                               wk.Name [Walker Name],
+                                               d.Name [Dog Name]
+                                      FROM Walk w
+                                      INNER JOIN Walker wk
+                                      ON w.WalkerId = wk.Id
+                                      INNER JOIN Dog d 
+                                      ON w.DogId = d.Id
+                                      WHERE w.Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    Walk walk = null;
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            if (walk == null)
+                            {
+                                walk = new Walk
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                    Date = reader.GetDateTime(reader.GetOrdinal("Date")),
+                                    WalkerId = reader.GetInt32(reader.GetOrdinal("WalkerId")),
+                                    DogId = reader.GetInt32(reader.GetOrdinal("DogId")),
+                                    Duration = reader.GetInt32(reader.GetOrdinal("Duration")),
+                                };
+                            }
+                            // If there is a DogId in the database:
+                            if (!reader.IsDBNull(reader.GetOrdinal("DogId")))
+                            {
+                                walk.Dog = new Dog
+                                {
+                                    Name = reader.GetString(reader.GetOrdinal("Dog Name")),
+                                };
+                            }
+                            // If there is a WalkerId in the database:
+                            if (!reader.IsDBNull(reader.GetOrdinal("WalkerId")))
+                            {
+                                walk.Walker = new Walker
+                                {
+                                    Name = reader.GetString(reader.GetOrdinal("Walker Name"))
+                                };
+                            }
+                            else
+                            {
+                                return null;
+                            }
+                        }
+
+                        return walk;
+                    }
+                }
+            }
+        }
+
+        // CREATE/ADD Walk:
+        public void AddWalk(Walk walk)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO Walk (Date, WalkerId, DogId, Duration)
+                    OUTPUT INSERTED.ID
+                    VALUES (@date, @walkerId, @dogId, @duration);
+                ";
+
+                    cmd.Parameters.AddWithValue("@date", walk.Date);
+                    cmd.Parameters.AddWithValue("@walkerId", walk.WalkerId);
+                    cmd.Parameters.AddWithValue("@dogId", walk.DogId);
+                    cmd.Parameters.AddWithValue("@duration", walk.Duration);
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    walk.Id = id;
+                }
+            }
+        }
+
+
     }
 }
