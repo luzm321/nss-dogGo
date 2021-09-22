4000dc7441b4c8bed69a31c74cfb929518a825c9
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-09-22 13:52:07 -0500

    created Walk.cs file and WalkerProfileViewModel.cs file to represent the walks associated with each walker in the walker profile details page

---
+++ Models/Walk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Models
+{
+    public class Walk
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int WalkerId { get; set; }
+        public int DogId { get; set; }
+        public int Duration { get; set; }
+        // may need to make a list of duration times to calculate the total walk time of each walker   
+    }
+}
