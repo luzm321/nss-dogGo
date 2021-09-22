ea2319284e8c6c8b57dbcd360cd3125086a1381a
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-09-22 14:59:16 -0500

    added method to delete a walk; added all CRUD methods in WalkRepository class

--- Repositories/WalkRepository.cs
+++ Repositories/WalkRepository.cs
@@ -204,5 +204,26 @@ namespace DogGo.Repositories
                 }
             }
         }
+
+        // DELETE: Walk
+        public void DeleteWalk(int walkId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            DELETE FROM Walks
+                            WHERE Id = @id
+                        ";
+
+                    cmd.Parameters.AddWithValue("@id", walkId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
